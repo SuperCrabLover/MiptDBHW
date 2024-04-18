6105d953f1e3aa8d5241676428840c671714c38f
SuperCrabLover <molozhavenko.aa@phystech.edu>
2024-04-18 15:51:27 +0300

    fixes

--- experiments/redis/insert_strings.py
+++ experiments/redis/insert_strings.py
@@ -3,25 +3,23 @@ import redis
 import time
 
 if __name__ == '__main__':
+	DEBUG_UPLOAD_LIMIT = 1000
 
-    DEBUG_UPLOAD_LIMIT = 1000
+	r = redis.StrictRedis(host='localhost', port=6379, db=1)
 
-    r = redis.StrictRedis(host='localhost', port=6379, db=1)
+	print('Upload json as true-string.')
+	with open('data.json', encoding='cp1251') as input_file:
+	test_data = json.load(input_file)
+	count = len(test_data)
+	total_time = 0
 
-    print('Upload json as true-string.')
-    with open('data.json', encoding='cp1251') as input_file:
-        test_data = json.load(input_file)
-        count = len(test_data)
-        start = time.time()
-
-        index = 0
-        for index, data in enumerate(test_data):
-            if DEBUG_UPLOAD_LIMIT and index == DEBUG_UPLOAD_LIMIT:
-                break
-            value = str(data).lower().encode('utf-8')
-            r.set('obj:%s' % index, value)
-            r.save()
-
-        end = time.time()
-        print('\t', 'Were upload:', index, 'units')
-        print('\t', 'Time left:', end - start, 'sec')
+	for i, data in enumerate(test_data):
+		if i == DEBUG_UPLOAD_LIMIT:
+			break
+		value = str(data).lower().encode('utf-8')
+		start = time.time()
+		r.set('obj:%s' % index, value)
+		r.save()
+		end = time.time()
+		total_time += end - start
+	print(f'{i, total_time =}')
