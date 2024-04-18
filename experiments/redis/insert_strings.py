cd9e41c05eff1175c851e3fac194417e7dfeb42d
SuperCrabLover <molozhavenko.aa@phystech.edu>
2024-04-18 16:27:33 +0300

    working

--- experiments/redis/insert_strings.py
+++ experiments/redis/insert_strings.py
@@ -3,23 +3,21 @@ import redis
 import time
 
 if __name__ == '__main__':
-	DEBUG_UPLOAD_LIMIT = 1000
 
 	r = redis.StrictRedis(host='localhost', port=6379, db=1)
-
-	print('Upload json as true-string.')
 	with open('large-file.json') as input_file:
 		test_data = json.load(input_file)
-	count = len(test_data)
 	total_time = 0
 
+	print('Please stand by.')
 	for i, data in enumerate(test_data):
-		if i == DEBUG_UPLOAD_LIMIT:
-			break
+
 		value = str(data).lower().encode('utf-8')
 		start = time.time()
-		r.set('obj:%s' % i, value)
-		r.save()
+		flag = r.set(f'obj:{i}', value)
 		end = time.time()
 		total_time += end - start
-	print(f'{i, total_time =}')
+		if flag is False:
+			raise Exception('Set error! Aborting')
+	print(f'amount of "sets" = {i}, elapsed time = {total_time} sec.')
+	
