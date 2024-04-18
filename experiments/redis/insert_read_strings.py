78db12f8c2a25d7530713232aa9e6d40bc1eb03a
SuperCrabLover <molozhavenko.aa@phystech.edu>
2024-04-18 17:00:45 +0300

    a masterpiece

--- experiments/redis/insert_read_strings.py
+++ experiments/redis/insert_read_strings.py
@@ -1,30 +1,40 @@
 import json
 import redis
 import time
+def redis_set(r, key:str, val:str) -> bool:
+	return r.set(key, val)
+
+def redis_get(r, key:str, val:str = None) -> str:
+	return r.get(key)
+
+def conduct_exp(exp_am:int, redis_fun, r) -> [int, float]:
+	total_times = 0
+	for _ in range(exp_am):
+		r.flushdb()
+		exp_time = 0
+		for i, data in enumerate(test_data):
+			value = str(data).lower().encode('utf-8')
+			start = time.time()
+			flag = redis_fun(r, f'obj:{i}', value)
+			end = time.time()
+			exp_time += end - start
+			if flag is False:
+				raise Exception('Error! Aborting.')
+		total_times += exp_time
+	return i, total_times / exp_am
 
 if __name__ == '__main__':
-	r = redis.StrictRedis(host='localhost', port=6379, db=1)
+	r = redis.StrictRedis(host='localhost', port=6379, db=0)
 	with open('large-file.json') as input_file:
 		test_data = json.load(input_file)
-	total_time = 0
+	exp_am = 10
 
+	print(f'Mean of {exp_am} experiments.')
 	print('Please stand by. Setting.')
-	for i, data in enumerate(test_data):
-		value = str(data).lower().encode('utf-8')
-		start = time.time()
-		flag = r.set(f'obj:{i}', value)
-		end = time.time()
-		total_time += end - start
-		if flag is False:
-			raise Exception('Set error! Aborting')
-	print(f'amount of "sets" = {i}, "sets" elapsed time = {total_time} sec.')
+	i, res = conduct_exp(exp_am, redis_set, r)
+	print(f'Amount of "sets" per experiment = {i}, Mean "sets" elapsed time = {res} sec.')
 
-	total_time = 0
 	print('Please stand by. Reading.')
-	for i, _ in enumerate(test_data):
-		start = time.time()
-		__ = r.get(f'obj:{i}')
-		end = time.time()
-		total_time += end - start
-	print(f'amount of "gets" = {i}, "gets" elapsed time = {total_time} sec.')
+	i, res = conduct_exp(exp_am, redis_get, r)
+	print(f'Amount of "gets" per experiment = {i}, Mean "gets" elapsed time = {res} sec.')
 	
