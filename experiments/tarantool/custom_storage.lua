8173224eb2b68c28dd355c11bc9b79a9d6731530
SuperCrabLover <molozhavenko.aa@phystech.edu>
2024-04-29 22:20:20 +0300

    users!

--- experiments/tarantool/custom_storage.lua
+++ experiments/tarantool/custom_storage.lua
@@ -1,21 +1,21 @@
-box.schema.create_space('bands', {
+box.schema.create_space('users', {
     format = {
         { name = 'id', type = 'unsigned' },
         { name = 'bucket_id', type = 'unsigned' },
-        { name = 'band_name', type = 'string' },
-        { name = 'year', type = 'unsigned' }
+        { name = 'balance', type = 'double' },
+        { name = 'rub_per_sec', type = 'double' }
     },
     if_not_exists = true
 })
-box.space.bands:create_index('id', { parts = { 'id' }, if_not_exists = true })
-box.space.bands:create_index('bucket_id', { parts = { 'bucket_id' }, unique = false, if_not_exists = true })
-function insert_band(id, bucket_id, band_name, year)
-    box.space.bands:insert({ id, bucket_id, band_name, year })
+box.space.users:create_index('id', { parts = { 'id' }, if_not_exists = true })
+box.space.users:create_index('bucket_id', { parts = { 'bucket_id' }, unique = false, if_not_exists = true })
+function insert_user(id, bucket_id, balance, rub_per_sec)
+    box.space.users:insert({ id, bucket_id, balance, rub_per_sec })
 end
-function get_band(id)
-    local tuple = box.space.bands:get(id)
+function get_user(id)
+    local tuple = box.space.users:get(id)
     if tuple == nil then
         return nil
     end
-    return { tuple.id, tuple.band_name, tuple.year }
+    return { tuple.id, tuple.balance, tuple.rub_per_sec }
 end
